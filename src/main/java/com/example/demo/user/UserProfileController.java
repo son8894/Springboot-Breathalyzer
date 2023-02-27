ba94aabff1b0291ce2e7a6e4ab838feabb598696
son8894 <108383042+son8894@users.noreply.github.com>
2023-02-27 15:06:03 +0900

    Update UserProfileController.java

--- src/main/java/com/example/demo/user/UserProfileController.java
+++ src/main/java/com/example/demo/user/UserProfileController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.user.service.UserProfileMapper;
 
+@CrossOrigin(origin="*", allowedHeaders = "*")
 @RestController
 public class UserProfileController {
 
