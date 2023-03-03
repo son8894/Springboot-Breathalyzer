895ba30db8ec9967dde92c21b2ed7503c5063e16
son8894 <108383042+son8894@users.noreply.github.com>
2023-03-03 18:07:09 +0900

    Update UserProfileController.java

--- src/main/java/com/example/demo/user/UserProfileController.java
+++ src/main/java/com/example/demo/user/UserProfileController.java
@@ -65,5 +65,83 @@ public class UserProfileController {
 	@RequestParam("name") String name, @RequestParam("phone") String phone, @RequestParam("nickName") String nickName) {
 	    mapper.insertUserProfile(id, userId, password, name, phone, nickName);
 	}
+	
+	
+	public UserProfile(String userId, String password) {
+    this.userId = userId;
+    this.password = password;
+}
+
+java
+Copy code
+@Select("SELECT * FROM UserProfile WHERE userId=#{userId} AND password=#{password}")
+UserProfile getUserProfileByUserIdAndPassword(@Param("userId") String userId, @Param("password") String password);
+
+java
+Copy code
+@PostMapping("/user/login")
+public boolean login(@RequestParam("userId") String userId, @RequestParam("password") String password) {
+    UserProfile userProfile = mapper.getUserProfileByUserIdAndPassword(userId, password);
+    return userProfile != null;
+}
+
+java
+Copy code
+package com.example.demo.user;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.user.service.UserProfileMapper;
+
+@CrossOrigin(origins = {"*"})
+@RestController
+public class UserProfileController {
+
+    public UserProfileMapper mapper;
+
+    public UserProfileController(UserProfileMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @GetMapping("/user/{id}")
+    public UserProfile getUserProfile(@PathVariable("id") int id) {
+        return mapper.getUserProfile(id);
+    }
+
+    @GetMapping("/user/all")
+    public List<UserProfile> getUserProfileList() {
+        return mapper.getUserProfileList();
+    }
+
+    @PutMapping("/user/{id}")
+    public void putUserProfile(@PathVariable("id") int id, @RequestParam("userId") String userId, @RequestParam("password") String password, 
+        @RequestParam("name") String name, @RequestParam("phone") String phone, @RequestParam("nickName") String nickName) {
+        mapper.insertUserProfile(id, userId, password, name, phone, nickName);
+    }
+
+    @PostMapping("/user/{id}")
+    public void postUserProfile(@PathVariable("id") int id, @RequestParam("userId") String userId, @RequestParam("password") String password, 
+            @RequestParam("name") String name, @RequestParam("phone") String phone, @RequestParam("nickName") String nickName) {
+        mapper.updateUserProfile(id, userId, password, name, phone, nickName);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public void deleteUserProfile(@PathVariable("id") int id) {
+        mapper.deleteUserProfile(id);
+    }
+
+    @PostMapping("/user/login")
+    public boolean login(@RequestParam("userId") String userId, @RequestParam("password") String password) {
+        UserProfile userProfile = mapper.getUserProfileByUserIdAndPassword(userId, password);
+       
 	*/
 }
