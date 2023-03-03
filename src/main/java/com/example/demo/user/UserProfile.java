888f7cc5b7896113cdaa09aaa24822f93d1f588b
son8894 <108383042+son8894@users.noreply.github.com>
2023-03-03 16:42:08 +0900

    Update UserProfile.java

--- src/main/java/com/example/demo/user/UserProfile.java
+++ src/main/java/com/example/demo/user/UserProfile.java
@@ -2,14 +2,16 @@ package com.example.demo.user;
 
 public class UserProfile {
 	private String id;
+	private String userId;
 	private String password;
 	private String name;
 	private String phone;
 	private String nickName;
 	
-	public UserProfile(String id, String password, String name, String phone, String nickName) {
+	public UserProfile(int id, String userId, String password, String name, String phone, String nickName) {
 		super();
 		this.id = id;
+		this.userId = userId;
 		this.password = password;
 		this.name = name;
 		this.phone = phone;
@@ -19,7 +21,13 @@ public class UserProfile {
 	public String getId() {
 		return id;
 	}
-	public void setId(String id) {
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
 		this.id = id;
 	}
 	public String getPassword() {
