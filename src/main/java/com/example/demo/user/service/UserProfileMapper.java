60700afef97604681347ca6c289a3645cc47e5de
son8894 <108383042+son8894@users.noreply.github.com>
2023-03-03 17:03:52 +0900

    Update UserProfileMapper.java

--- src/main/java/com/example/demo/user/service/UserProfileMapper.java
+++ src/main/java/com/example/demo/user/service/UserProfileMapper.java
@@ -15,19 +15,19 @@ import com.example.demo.user.UserProfile;
 public interface UserProfileMapper {
 	
 	@Select("SELECT * FROM UserProfile WHERE id=#{id}")
-	UserProfile getUserProfile(@Param("id") String id);
+	UserProfile getUserProfile(@Param("id") int id);
 
 	@Select("SELECT * FROM UserProfile")
 	List<UserProfile> getUserProfileList();
 	
-	@Insert("INSERT INTO UserProfile VALUES(#{id},#{password},#{name},#{phone},#{nickName})")
-	int insertUserProfile(@Param("id") String id, @Param("password") String password, @Param("name") String name,
+	@Insert("INSERT INTO UserProfile VALUES(#{id},#{userId},#{password},#{name},#{phone},#{nickName})")
+	int insertUserProfile(@Param("id") int id, @Param("userId") String userId, @Param("password") String password, @Param("name") String name,
 			@Param("phone") String phone, @Param("nickName") String nickName);
 	
-	@Update("UPDATE UserProfile SET password=#{password}, name=#{name}, phone=#{phone}, nickName=#{nickName} WHERE id=#{id})")
-	int updateUserProfile(@Param("id") String id, @Param("password") String password, @Param("name") String name,
+	@Update("UPDATE UserProfile SET userId=#{userId}, password=#{password}, name=#{name}, phone=#{phone}, nickName=#{nickName} WHERE id=#{id})")
+	int updateUserProfile(@Param("id") int id, @Param("userId") String userId, @Param("password") String password, @Param("name") String name,
 			@Param("phone") String phone, @Param("nickName") String nickName);
 	
 	@Delete("DELETE FROM UserProfile WHERE id=#{id}")
-	int deleteUserProfile(@Param("id") String id);
+	int deleteUserProfile(@Param("id") int id);
 }
